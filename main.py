3fa0c353d975447a383bf8952271441f921f337f
tensaidevx <shubham.gupta.npd@gmail.com>
2025-10-12 23:08:05 +0530

    feature/update

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from enum import Enum 
+
+# Import Enum and create a sub-class that inherits from str and from Enum.
+
+class ModelName(str , Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 app = FastAPI()
 
 class Item(BaseModel):
@@ -22,3 +31,11 @@ def read_item(item_id:int, q:Union[str,None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id:int, item:Item):
     return {"item_price":item.price, "item_id":item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name:ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message":"Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message":"LeCNN all the images"}
+    return {"model_name": model_name, "message":"Have some residuals"}
