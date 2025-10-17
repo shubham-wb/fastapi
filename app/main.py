7b05e298da83e9787dcafd57336c5e560e3aa3b9
tensaidevx <shubham.gupta.npd@gmail.com>
2025-10-17 07:18:29 +0530

    feature/update

--- app/main.py
+++ app/main.py
@@ -8,38 +8,13 @@ from enum import Enum
 
 # Import Enum and create a sub-class that inherits from str and from Enum.
 
-class ModelName(str , Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name : str
-    price: float
-    is_offer:Union[bool, None] = None
 
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id:int, q:Union[str,None] = None):
-    return {"item_id":item_id, "q":q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id:int, item:Item):
-    return {"item_price":item.price, "item_id":item_id}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name:ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message":"Deep Learning FTW!"}
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message":"LeCNN all the images"}
-    return {"model_name": model_name, "message":"Have some residuals"}
 
 
 class Post(BaseModel):
@@ -51,10 +26,6 @@ class Post(BaseModel):
 my_posts = [{"title":"title of post 1","content":"content of post 1", "id":1}]
 
 
-@app.get("/hello") 
-# decorator
-def say_hello():
-    return {"message":"Hello World"}
 
 @app.get("/posts")
 def get_posts():
@@ -88,4 +59,15 @@ def delete_post(id:int):
         if post['id'] == id:
             my_posts.pop(i)
             return Response(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+
+
+@app.put("/posts/{id}")
+def update_post(id:int, payload:Post):
+    for i, post in enumerate(my_posts):  #enumerate gives a tuple  (index, post)
+        if post['id'] == id:
+            post_dict = payload.dict()
+            post_dict['id'] = id
+            my_posts[i] = post_dict
+            return {"message":"Successfully updated post!", "data":post_dict}    
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
