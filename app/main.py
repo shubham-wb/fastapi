ab4a462bc67b42c6af9f3e94e1fa94226a20147a
tensaidevx <shubham.gupta.npd@gmail.com>
2025-10-18 20:07:08 +0530

    feature/get-post-id

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from http.client import HTTPException
 from typing import Optional, Union
+import os
 
 from fastapi import Body, FastAPI, Response, status
 from pydantic import BaseModel
 from random import randrange
 from enum import Enum 
+import psycopg2
+import time 
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
 
 # Import Enum and create a sub-class that inherits from str and from Enum.
+
+# Load environment variables
+load_dotenv()
 
 
 app = FastAPI()
@@ -20,16 +29,31 @@ def read_root():
 class Post(BaseModel):
     title: str
     content: str
-    rating: Optional[int] = None
     published: bool = True
 
 my_posts = [{"title":"title of post 1","content":"content of post 1", "id":1}]
 
-
+while True:
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            cursor_factory=RealDictCursor
+        )
+        cursor = conn.cursor()
+        print("connected to database")
+        break
+    except Exception as error:
+        print("Error while connecting to database", error)
+        time.sleep(2)
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"This is your posts"}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data":posts}
 
 @app.get("/posts/{id}")
 def get_post(id:int,response:Response):
@@ -45,12 +69,15 @@ def create_posts(payload:Post):
     post_dict = payload.dict()
     post_dict['id'] = randrange(0,1000000)
     my_posts.append(post_dict)
-    
-    return {"message":"Successfully created post!", "data":post_dict}    
+    cursor.execute("""INSERT INTO posts(title,content,published,created_at) VALUES(%s,%s,%s,%s)""", (post_dict['title'], post_dict['content'], post_dict['published'], time.strftime('%Y-%m-%d %H:%M:%S')))
+    conn.commit()
+    return {"message":"Successfully created post!", "data":post_dict}
 
-@app.get("/posts/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts)-1]
+@app.get("/posts/{id}")
+def get_post(id:int):
+    print("ID is ", id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
+    post = cursor.fetchone()
     return {"post_detail":post}
 
 @app.delete("/posts/{id}")
@@ -71,3 +98,6 @@ def update_post(id:int, payload:Post):
             my_posts[i] = post_dict
             return {"message":"Successfully updated post!", "data":post_dict}    
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+
+
+#database integration
